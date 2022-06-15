89442ba750c81e0bdfb6c2fcbbfc415545e66964
champkeh <champion.ge@gmail.com>
2022-06-16 01:59:13 +0800

    build

---
+++ build/assets/idcard.9f5cef36.js
@@ -0,0 +1,1 @@
+import{_ as a,o as e,c as t}from"./index.131b82aa.js";const n={};function c(_,r){return e(),t("main",null,"\u656C\u8BF7\u671F\u5F85~")}var o=a(n,[["render",c],["__scopeId","data-v-6a7f3954"]]);export{o as default};
