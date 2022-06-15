c1b7a28773352d01f4f1379377be6d114b82cf42
champkeh <champion.ge@gmail.com>
2022-06-15 13:50:25 +0800

    搭建基础框架

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <div>{{ name }}</div>
+    <button @click="toggleName">switch</button>
+  </main>
+</template>
+
+<script setup>
+import {ref, computed} from 'vue'
+
+const on = ref(true)
+const name = computed(() => on.value ? 'Deno' : 'Vue3')
+
+function toggleName() {
+  on.value = !on.value
+}
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+div {
+  margin: 20px;
+}
+button {
+  width: 80%;
+  height: 40px;
+}
+</style>
